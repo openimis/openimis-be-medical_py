870c26fa9fba39758fc00f5644acac2210dddd13
Eric Darchis <darchis@pobox.com>
2019-08-08 10:09:57 +0200

    Add pagination, standardise on *GQLType names

--- medical/schema.py
+++ medical/schema.py
@@ -2,27 +2,42 @@ import re
 from django.db.models import Q
 import graphene
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
 from .models import Item, Service
 
 
-class ItemGraphQLType(DjangoObjectType):
+class ItemGQLType(DjangoObjectType):
     class Meta:
         model = Item
+        filter_fields = {
+            'id': ['exact'],
+            'code': ['exact', 'icontains', 'istartswith'],
+            'name': ['exact', 'icontains', 'istartswith'],
+        }
+        filter_fields = ()
+        interfaces = (graphene.relay.Node,)
 
 
-class ServiceGraphQLType(DjangoObjectType):
+class ServiceGQLType(DjangoObjectType):
     class Meta:
         model = Service
+        filter_fields = {
+            'id': ['exact'],
+            'code': ['exact', 'icontains', 'istartswith'],
+            'name': ['exact', 'icontains', 'istartswith'],
+        }
+        interfaces = (graphene.relay.Node,)
 
 
 class Query(graphene.ObjectType):
-    medical_items = graphene.List(
-        ItemGraphQLType,
+    medical_items = DjangoFilterConnectionField(
+        ItemGQLType,
         qry=graphene.String()
     )
 
-    medical_services = graphene.List(
-        ServiceGraphQLType,
+    medical_services = DjangoFilterConnectionField(
+        ServiceGQLType,
         qry=graphene.String()
     )
 
