87048ff6bf97d8071e50c4b73438a2f2d4ca40b4
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-10 13:38:27 +0200

    str(Diagnosis, Item & Service), for Claim pdf,...

--- medical/models.py
+++ medical/models.py
@@ -2,16 +2,22 @@ import uuid
 from django.db import models
 from core import fields
 
+
 class Diagnosis(models.Model):
     id = models.AutoField(db_column='ICDID', primary_key=True)
-    legacy_id = models.IntegerField(db_column='LegacyID', blank=True, null=True)
+    legacy_id = models.IntegerField(
+        db_column='LegacyID', blank=True, null=True)
     code = models.CharField(db_column='ICDCode', max_length=6)
     name = models.CharField(db_column='ICDName', max_length=255)
     validity_from = fields.DateTimeField(db_column='ValidityFrom')
-    validity_to = fields.DateTimeField(db_column='ValidityTo', blank=True, null=True)
+    validity_to = fields.DateTimeField(
+        db_column='ValidityTo', blank=True, null=True)
 
     audit_user_id = models.IntegerField(db_column='AuditUserID')
     # row_id = models.BinaryField(db_column='RowID', blank=True, null=True)
+
+    def __str__(self):
+        return self.code + " " + self.name
 
     class Meta:
         managed = False
@@ -20,20 +26,29 @@ class Diagnosis(models.Model):
 
 class Item(models.Model):
     id = models.AutoField(db_column='ItemID', primary_key=True)
-    uuid = models.CharField(db_column='ItemUUID', max_length=36, default=uuid.uuid4, unique = True)
-    legacy_id = models.IntegerField(db_column='LegacyID', blank=True, null=True)
+    uuid = models.CharField(db_column='ItemUUID',
+                            max_length=36, default=uuid.uuid4, unique=True)
+    legacy_id = models.IntegerField(
+        db_column='LegacyID', blank=True, null=True)
     code = models.CharField(db_column='ItemCode', max_length=6)
     name = models.CharField(db_column='ItemName', max_length=100)
     type = models.CharField(db_column='ItemType', max_length=1)
-    package = models.CharField(db_column='ItemPackage', max_length=255, blank=True, null=True)
-    price = models.DecimalField(db_column='ItemPrice', max_digits=18, decimal_places=2)
+    package = models.CharField(
+        db_column='ItemPackage', max_length=255, blank=True, null=True)
+    price = models.DecimalField(
+        db_column='ItemPrice', max_digits=18, decimal_places=2)
     care_type = models.CharField(db_column='ItemCareType', max_length=1)
-    frequency = models.SmallIntegerField(db_column='ItemFrequency', blank=True, null=True)
+    frequency = models.SmallIntegerField(
+        db_column='ItemFrequency', blank=True, null=True)
     patient_category = models.SmallIntegerField(db_column='ItemPatCat')
     validity_from = fields.DateTimeField(db_column='ValidityFrom')
-    validity_to = fields.DateTimeField(db_column='ValidityTo', blank=True, null=True)
+    validity_to = fields.DateTimeField(
+        db_column='ValidityTo', blank=True, null=True)
     audit_user_id = models.IntegerField(db_column='AuditUserID')
     # row_id = models.BinaryField(db_column='RowID', blank=True, null=True)
+
+    def __str__(self):
+        return self.code + " " + self.name
 
     class Meta:
         managed = False
@@ -42,23 +57,34 @@ class Item(models.Model):
 
 class Service(models.Model):
     id = models.AutoField(db_column='ServiceID', primary_key=True)
-    uuid = models.CharField(db_column='ServiceUUID', max_length=36, default=uuid.uuid4, unique = True)
-    legacy_id = models.IntegerField(db_column='LegacyID', blank=True, null=True)
-    category = models.CharField(db_column='ServCategory', max_length=1, blank=True, null=True)
+    uuid = models.CharField(db_column='ServiceUUID',
+                            max_length=36, default=uuid.uuid4, unique=True)
+    legacy_id = models.IntegerField(
+        db_column='LegacyID', blank=True, null=True)
+    category = models.CharField(
+        db_column='ServCategory', max_length=1, blank=True, null=True)
     code = models.CharField(db_column='ServCode', max_length=6)
     name = models.CharField(db_column='ServName', max_length=100)
     type = models.CharField(db_column='ServType', max_length=1)
     level = models.CharField(db_column='ServLevel', max_length=1)
-    price = models.DecimalField(db_column='ServPrice', max_digits=18, decimal_places=2)
+    price = models.DecimalField(
+        db_column='ServPrice', max_digits=18, decimal_places=2)
     care_type = models.CharField(db_column='ServCareType', max_length=1)
-    frequency = models.SmallIntegerField(db_column='ServFrequency', blank=True, null=True)
+    frequency = models.SmallIntegerField(
+        db_column='ServFrequency', blank=True, null=True)
     patient_category = models.SmallIntegerField(db_column='ServPatCat')
 
-    validity_from = fields.DateTimeField(db_column='ValidityFrom', blank=True, null=True)
-    validity_to = fields.DateTimeField(db_column='ValidityTo', blank=True, null=True)
+    validity_from = fields.DateTimeField(
+        db_column='ValidityFrom', blank=True, null=True)
+    validity_to = fields.DateTimeField(
+        db_column='ValidityTo', blank=True, null=True)
 
-    audit_user_id = models.IntegerField(db_column='AuditUserID', blank=True, null=True)
+    audit_user_id = models.IntegerField(
+        db_column='AuditUserID', blank=True, null=True)
     # row_id = models.BinaryField(db_column='RowID', blank=True, null=True)
+
+    def __str__(self):
+        return self.code + " " + self.name
 
     class Meta:
         managed = False
