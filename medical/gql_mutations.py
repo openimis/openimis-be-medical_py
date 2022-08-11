e76da574765ef6882234efacd7fbe27dd9d7cd5c
maxime ngoe <maxime.ngoe@gmail.com>
2022-08-11 18:51:56 +0100

    ajustement gql_mutation pour correction Item

--- medical/gql_mutations.py
+++ medical/gql_mutations.py
@@ -260,7 +260,7 @@ class DeleteServiceMutation(OpenIMISMutation):
 
 
 class ItemInputType(ItemOrServiceInputType):
-    package = graphene.String(required=True)
+    package = graphene.String()
     quantity = graphene.Decimal()
 
 class CreateItemMutation(CreateOrUpdateItemOrServiceMutation):
